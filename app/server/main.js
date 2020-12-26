28651fe01d6d93f71f554a23933c74060c21d682
Gustavo Cunha <gustavo.cunha@ibm.com>
2020-12-26 19:25:41 -0300

    express setup

--- app/server/main.js
+++ app/server/main.js
@@ -1,2 +1,15 @@
 // ESM syntax is supported.
-export {}
+import express from 'express';
+import { presignedRoutes } from './routes';
+
+const PORT = 3030;
+
+const app = express();
+
+app.use('/', (req, res) => res.json('API is up and running!'));
+
+app.use('/api/presigned', presignedRoutes);
+
+app.listen(PORT, () => {
+    console.log(`API listening on port ${PORT}`);
+});
