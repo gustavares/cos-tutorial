4a89c2f526628c92fdfbda3501178fa4c7f5ce92
Gustavo Cunha <gustavo.cunha@ibm.com>
2021-01-19 23:24:44 -0300

    multipart upload

--- app/web/src/components/uploadInput.js
+++ app/web/src/components/uploadInput.js
@@ -1,6 +1,8 @@
 import { api, BUCKET_NAME } from '../api';
 import { useState } from 'react';
 
+const FILE_CHUNK_SIZE = 1024 * 1024 * 5;
+
 function UploadInput({fileList, setFileList }) {
     const [ selectedFile, setSelectedFile ] = useState();
     const [ uploadProgress, setUploadProgress ] = useState();
@@ -18,6 +20,20 @@ function UploadInput({fileList, setFileList }) {
             console.log(e);
         }
     }
+
+    async function fetchUploadIdAndParts(filename, numberOfParts) {
+        try {
+            const response = await api.get(`/buckets/${BUCKET_NAME}/files/${filename}/presigned/upload/multipart?parts=${numberOfParts}`);
+
+            if (response.status >= 400) {
+                throw response.data;
+            }
+
+            return response.data;
+        } catch(e) {
+            console.log(e);
+        }
+    }
     
     function handleInputChange(event) {
         const file = event.target.files[0];
@@ -27,31 +43,101 @@ function UploadInput({fileList, setFileList }) {
         }
     }
 
-    async function uploadFile() {
-        if (selectedFile === undefined) {
-            alert('Please select a file first.');
-            return;
+    function getFileChunks(file) {
+        const chunks = [];
+        const finalPointer = file.size;
+        let startPointer = 0;
+
+        while (startPointer < finalPointer) {
+            const newStartPointer = startPointer + FILE_CHUNK_SIZE;
+            
+            const slice = file.slice(startPointer, newStartPointer);
+            console.log(slice);
+            chunks.push(slice);
+            
+            startPointer = newStartPointer; 
         }
 
+        return chunks;
+    }
+
+    async function completeMultipartUpload(filename, uploadId, partsETags) {
+
         try {
-            const url = await fetchUploadUrl(selectedFile.name);
-            const response = await api.put(url, selectedFile, {
-                onUploadProgress: (progressEvent) => {
-                    setUploadProgress(Math.round((progressEvent.loaded * 100) / progressEvent.total));
-                }
+            const response = await api.post(`/buckets/${BUCKET_NAME}/files/${filename}/presigned/upload/multipart`, {
+                uploadId,
+                partsETags
             });
 
             if (response.status >= 400) {
                 throw response.data;
             }
 
-            setFileList([...fileList, selectedFile.name]);
-            setUploadProgress(undefined);
-            
-            alert(`${selectedFile.name} uploaded successfully!`);
+            alert(`Completed multipart upload for ${filename}`);
         } catch (e) {
             console.log(e);
         }
+    }
+    
+    function uploadProgressHandler(progressEvent, total) {
+        setUploadProgress(Math.round((progressEvent.loaded * 100) / total))
+    }
+
+    /**
+     * @param {File} file 
+     */
+    async function multipartUpload(file) {
+        const chunks = getFileChunks(file);
+        const { uploadId, parts } = await fetchUploadIdAndParts(file.name, chunks.length);
+
+        const promises = [];
+
+        parts.map(({ url, part }) => {
+            promises.push(api.put(url, chunks[part - 1], {
+                onUploadProgress: (e) => uploadProgressHandler(e, file.size)
+            }));
+        });
+
+        const response = await Promise.all(promises);
+
+        const partsETags = response.map((part, index) => {
+            return {
+                ETag: part.headers.etag,
+                PartNumber: (index + 1)
+            }
+        });
+        completeMultipartUpload(file.name, uploadId, partsETags);
+    }
+
+    async function onUploadButtonClick() {
+        if (selectedFile === undefined) {
+            alert('Please, first select a file.');
+            return;
+        }
+
+        if (selectedFile.size > FILE_CHUNK_SIZE) {
+            await multipartUpload(selectedFile);
+        }
+
+        // try {
+        //     const url = await fetchUploadUrl(selectedFile.name);
+        //     const response = await api.put(url, selectedFile, {
+        //         onUploadProgress: (progressEvent) => {
+        //             setUploadProgress(Math.round((progressEvent.loaded * 100) / progressEvent.total));
+        //         }
+        //     });
+
+        //     if (response.status >= 400) {
+        //         throw response.data;
+        //     }
+
+            setFileList([...fileList, selectedFile.name]);
+            setUploadProgress(undefined);
+            
+        //     alert(`${selectedFile.name} uploaded successfully!`);
+        // } catch (e) {
+        //     console.log(e);
+        // }
 
         document.getElementById('fileInput').value = '';
     }
@@ -60,7 +146,7 @@ function UploadInput({fileList, setFileList }) {
         <div>
             <input id="fileInput" type="file" onChange={handleInputChange}/>
 
-            <button onClick={uploadFile}>Upload file</button>
+            <button onClick={onUploadButtonClick}>Upload file</button>
 
             {uploadProgress ? (
                 <span style={{ marginLeft: "30px"}}>
