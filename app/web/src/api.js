dc62a97103cf1b7cda956be148327df294918b06
Gustavo Cunha <gustavo.cunha@ibm.com>
2021-01-19 23:23:49 -0300

    setting content type

--- app/web/src/api.js
+++ app/web/src/api.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: process.env.REACT_APP_API_URL
+    baseURL: process.env.REACT_APP_API_URL,
+    headers: {
+        'Content-Type': 'application/json'
+    }
 });
 
 export const BUCKET_NAME = 'cos-tutorial-presigned';
